bc4539b4ca0aefc8bd7e58aeb2f1ccb25eee2b19
Guus Lieben <guus@xendox.com>
2019-11-13 13:49:05 +0100

    Update MongoService

--- services/mongoService.js
+++ services/mongoService.js
@@ -1,4 +1,21 @@
-const db = require('mongoose');
+import mongo from 'mongoose';
+const Schema = mongo.Schema;
+const ObjectId = Schema.ObjectId;
+
+const db = mongo.connect('mongodb://localhost:27017/studdit', (err, response) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Connected to ' + db, ' + ', response);
+  }
+});
+
+const exampleSchema = Schema({
+    ownerId: ObjectId,
+    description: String
+});
+
+const exampleModel = mongo.model('exampleName', exampleSchema, 'exampleCollection')
 
 export function setup() {
     // Enjoy
