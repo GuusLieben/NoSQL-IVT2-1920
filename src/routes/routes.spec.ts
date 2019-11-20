e992a1b55278f46b9e2c18489bb5d26ba1128825
Tivawo <timvanwouwe@upcmail.nl>
2019-11-20 19:59:04 +0100

    updated tests

--- src/routes/routes.spec.ts
+++ src/routes/routes.spec.ts
@@ -1,6 +1,24 @@
 const chaiTester = require('chai');
 const chaiHttp = require('chai-http');
+const app = require('../app').app;
+const expect = chaiTester.expect;
 
 chaiTester.use(chaiHttp);
+
+
+describe('getUser test', () => {
+
+    it('should return user Guus', (done: any) => {
+        chaiTester.request(app)
+            .get('/Guus/')
+            .end(function (err: any, res: any) {
+                expect(err).to.be.null;
+                expect(res).to.have.status(200);
+            });
+    });
+})
+
+
+
 
 
