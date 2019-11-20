3f8a5bcbd3a6e073d2bb9fc52009e4c13c2d9ebe
Tivawo <timvanwouwe@upcmail.nl>
2019-11-20 15:49:44 +0100

    updated comment controller + routes

--- src/controllers/comment.controller.ts
+++ src/controllers/comment.controller.ts
@@ -1,10 +1,11 @@
 import {RepositoryInterface} from "../services/repository.interface";
 import {RepositoryMongodb} from "../services/repository.mongodb";
 import {logger} from "../app";
+import {commentController} from "./controllers.bin";
 
 const mongoRepository: RepositoryInterface = new RepositoryMongodb();
 
-export async function postComment (req: any, res: any) {
+export async function postComment(req: any, res: any) {
     logger.debug('> postComment was called');
     const username = req.body.username;
     const content = req.body.content;
@@ -18,5 +19,33 @@ export async function postComment (req: any, res: any) {
     } else {
         res.status(200).json({result: result.result});
         logger.color('green').debug('> postComment was successful');
+    }
+}
+
+export async function getComment(req: any, res: any) {
+    logger.debug('> getComment was called')
+    const commentId = req.params.id;
+    const result = await mongoRepository.getComment(commentId);
+
+    if (result.error) {
+        res.status(500).json({err: result.error});
+        logger.color('red').debug('> getComment went wrong!');
+    } else {
+        res.status(200).json({result: result.result});
+        logger.color('green').debug('> getComment was successful');
+    }
+}
+
+export async function deleteComment(req: any, res: any) {
+    logger.debug('> getComment was called')
+    const commentId = req.params.id;
+    const result = await mongoRepository.deleteComment(commentId);
+
+    if (result.error) {
+        res.status(500).json({err: result.error});
+        logger.color('red').debug('> deleteComment went wrong!');
+    } else {
+        res.status(200).json({result: result.result});
+        logger.color('green').debug('> deleteComment was successful');
     }
 }
--- src/routes/comment.routes.ts
+++ src/routes/comment.routes.ts
@@ -1,21 +1,24 @@
 import * as express from 'express';
 import {logger} from "../app";
 export const router = express.Router();
-const controller = require('../controllers/controllers.bin').commentController;
+const commentController = require('../controllers/controllers.bin').commentController;
+const threadController = require('../controllers/controllers.bin').threadController;
 
 // #MongoDB
 
-router.delete('/:id/', (req, res, next) => {
-
+router.delete('/:id/', (req, res) => {
+    commentController.deleteComment(req, res);
 });
 
-router.get('/:id/', (req, res, next) => {
+router.get('/:id/', (req, res) => {
     // Get by comment Id
     // Username, content, up-/downvotes
+    commentController.getComment(req, res);
 });
 
-router.get('/:id/comments/', (req, res, next) => {
+router.get('/:id/comments/', (req, res) => {
     // Get by comment Id
     // Username, content, up-/downvotes
+    threadController.getCommentsForThread(req, res);
 });
 
