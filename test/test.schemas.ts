878aaaf645faa19e9b69e9560c389f4e59d9ac33
Guus Lieben <guus@xendox.com>
2020-01-03 12:55:59 +0100

    Add all Schema tests

--- test/test.schemas.ts
+++ test/test.schemas.ts
@@ -1,6 +1,7 @@
 // @ts-ignore
 import {setProduction} from "../src/app";
-import {User,Thread,Comment} from '../src/schemas/schema.mongoDb';
+import {User,Thread,Comment, UserModel} from '../src/schemas/schema.mongoDb';
+import {mongoose} from '../src/services/service';
 
 const chai = require('chai');
 const mocha = require('mocha');
@@ -95,6 +96,302 @@ describe('User Schema', () => {
     it('should have the password property as String', (done: any) => {
         expect(User.obj.password).to.have.property('type');
         expect(User.obj.password.type).to.equal(String);
+        done();
+    });
+});
+
+//
+describe('Thread Schema', () => {
+    it('should have a user property', (done: any) => {
+        expect(Thread.obj).to.have.property('user');
+        done();
+    });
+});
+
+describe('Thread Schema', () => {
+    it('should have user as a required property', (done: any) => {
+        expect(Thread.obj.user).to.have.property('required');
+        expect(Thread.obj.user.required).to.equal(true);
+        done();
+    });
+});
+
+describe('Thread Schema', () => {
+    it('should have the user property as String', (done: any) => {
+        expect(Thread.obj.user).to.have.property('type');
+        expect(Thread.obj.user.type).to.equal(String);
+        done();
+    });
+});
+
+//
+describe('Thread Schema', () => {
+    it('should have a title property', (done: any) => {
+        expect(Thread.obj).to.have.property('title');
+        done();
+    });
+});
+
+describe('Thread Schema', () => {
+    it('should have title as a required property', (done: any) => {
+        expect(Thread.obj.title).to.have.property('required');
+        expect(Thread.obj.title.required).to.equal(true);
+        done();
+    });
+});
+
+describe('Thread Schema', () => {
+    it('should have the title property as String', (done: any) => {
+        expect(Thread.obj.title).to.have.property('type');
+        expect(Thread.obj.title.type).to.equal(String);
+        done();
+    });
+});
+
+//
+describe('Thread Schema', () => {
+    it('should have a content property', (done: any) => {
+        expect(Thread.obj).to.have.property('content');
+        done();
+    });
+});
+
+describe('Thread Schema', () => {
+    it('should have content as a required property', (done: any) => {
+        expect(Thread.obj.content).to.have.property('required');
+        expect(Thread.obj.content.required).to.equal(true);
+        done();
+    });
+});
+
+describe('Thread Schema', () => {
+    it('should have the content property as String', (done: any) => {
+        expect(Thread.obj.content).to.have.property('type');
+        expect(Thread.obj.content.type).to.equal(String);
+        done();
+    });
+});
+
+//
+describe('Thread Schema', () => {
+    it('should have a upvotedBy property', (done: any) => {
+        expect(Thread.obj).to.have.property('upvotedBy');
+        done();
+    });
+});
+
+describe('Thread Schema', () => {
+    it('should have upvotedBy with default value', (done: any) => {
+        expect(Thread.obj.upvotedBy).to.have.property('default');
+        expect(Thread.obj.upvotedBy.default).to.be.a('array');
+        done();
+    });
+});
+
+describe('Thread Schema', () => {
+    it('should have upvotedBy with type array', (done: any) => {
+        expect(Thread.obj.upvotedBy).to.have.property('type');
+        expect(Thread.obj.upvotedBy.type).to.be.a('array');
+        done();
+    });
+});
+
+describe('Thread Schema', () => {
+    it('should have upvotedBy with type array with rule type String', (done: any) => {
+        expect(Thread.obj.upvotedBy.type[0]).to.have.property('type');
+        expect(Thread.obj.upvotedBy.type[0].type).to.equal(String);
+        done();
+    });
+});
+
+//
+describe('Thread Schema', () => {
+    it('should have a downvotedBy property', (done: any) => {
+        expect(Thread.obj).to.have.property('downvotedBy');
+        done();
+    });
+});
+
+describe('Thread Schema', () => {
+    it('should have downvotedBy with default value', (done: any) => {
+        expect(Thread.obj.downvotedBy).to.have.property('default');
+        expect(Thread.obj.downvotedBy.default).to.be.a('array');
+        done();
+    });
+});
+
+describe('Thread Schema', () => {
+    it('should have downvotedBy with type array', (done: any) => {
+        expect(Thread.obj.downvotedBy).to.have.property('type');
+        expect(Thread.obj.downvotedBy.type).to.be.a('array');
+        done();
+    });
+});
+
+describe('Thread Schema', () => {
+    it('should have downvotedBy with type array with rule type String', (done: any) => {
+        expect(Thread.obj.downvotedBy.type[0]).to.have.property('type');
+        expect(Thread.obj.downvotedBy.type[0].type).to.equal(String);
+        done();
+    });
+});
+
+//
+describe('Comment Schema', () => {
+    it('should have a user property', (done: any) => {
+        expect(Comment.obj).to.have.property('user');
+        done();
+    });
+});
+
+describe('Comment Schema', () => {
+    it('should have user as a required property', (done: any) => {
+        expect(Comment.obj.user).to.have.property('required');
+        expect(Comment.obj.user.required).to.equal(true);
+        done();
+    });
+});
+
+describe('Comment Schema', () => {
+    it('should have the user property as String', (done: any) => {
+        expect(Comment.obj.user).to.have.property('type');
+        expect(Comment.obj.user.type).to.equal(String);
+        done();
+    });
+});
+
+//
+describe('Comment Schema', () => {
+    it('should have a content property', (done: any) => {
+        expect(Comment.obj).to.have.property('user');
+        done();
+    });
+});
+
+describe('Comment Schema', () => {
+    it('should have content as a required property', (done: any) => {
+        expect(Comment.obj.content).to.have.property('required');
+        expect(Comment.obj.content.required).to.equal(true);
+        done();
+    });
+});
+
+describe('Comment Schema', () => {
+    it('should have the content property as String', (done: any) => {
+        expect(Comment.obj.content).to.have.property('type');
+        expect(Comment.obj.content.type).to.equal(String);
+        done();
+    });
+});
+
+//
+describe('Comment Schema', () => {
+    it('should have a thread property', (done: any) => {
+        expect(Comment.obj).to.have.property('thread');
+        done();
+    });
+});
+
+describe('Comment Schema', () => {
+    it('should have thread as a reference property', (done: any) => {
+        expect(Comment.obj.thread).to.have.property('ref');
+        expect(Comment.obj.thread.ref).to.equal('Thread');
+        done();
+    });
+});
+
+describe('Comment Schema', () => {
+    it('should have the thread property as String', (done: any) => {
+        expect(Comment.obj.thread).to.have.property('type');
+        expect(Comment.obj.thread.type).to.equal(mongoose.Schema.ObjectId);
+        done();
+    });
+});
+
+//
+describe('Comment Schema', () => {
+    it('should have a comment property', (done: any) => {
+        expect(Comment.obj).to.have.property('comment');
+        done();
+    });
+});
+
+describe('Comment Schema', () => {
+    it('should have comment as a reference property', (done: any) => {
+        expect(Comment.obj.comment).to.have.property('ref');
+        expect(Comment.obj.comment.ref).to.equal('Comment');
+        done();
+    });
+});
+
+describe('Comment Schema', () => {
+    it('should have the comment property as String', (done: any) => {
+        expect(Comment.obj.comment).to.have.property('type');
+        expect(Comment.obj.comment.type).to.equal(mongoose.Schema.ObjectId);
+        done();
+    });
+});
+
+//
+describe('Comment Schema', () => {
+    it('should have a upvotedBy property', (done: any) => {
+        expect(Comment.obj).to.have.property('upvotedBy');
+        done();
+    });
+});
+
+describe('Comment Schema', () => {
+    it('should have upvotedBy with default value', (done: any) => {
+        expect(Comment.obj.upvotedBy).to.have.property('default');
+        expect(Comment.obj.upvotedBy.default).to.be.a('array');
+        done();
+    });
+});
+
+describe('Comment Schema', () => {
+    it('should have upvotedBy with type array', (done: any) => {
+        expect(Comment.obj.upvotedBy).to.have.property('type');
+        expect(Comment.obj.upvotedBy.type).to.be.a('array');
+        done();
+    });
+});
+
+describe('Comment Schema', () => {
+    it('should have upvotedBy with type array with rule type String', (done: any) => {
+        expect(Comment.obj.upvotedBy.type[0]).to.have.property('type');
+        expect(Comment.obj.upvotedBy.type[0].type).to.equal(String);
+        done();
+    });
+});
+
+//
+describe('Comment Schema', () => {
+    it('should have a downvotedBy property', (done: any) => {
+        expect(Comment.obj).to.have.property('downvotedBy');
+        done();
+    });
+});
+
+describe('Comment Schema', () => {
+    it('should have downvotedBy with default value', (done: any) => {
+        expect(Comment.obj.downvotedBy).to.have.property('default');
+        expect(Comment.obj.downvotedBy.default).to.be.a('array');
+        done();
+    });
+});
+
+describe('Comment Schema', () => {
+    it('should have downvotedBy with type array', (done: any) => {
+        expect(Comment.obj.downvotedBy).to.have.property('type');
+        expect(Comment.obj.downvotedBy.type).to.be.a('array');
+        done();
+    });
+});
+
+describe('Comment Schema', () => {
+    it('should have downvotedBy with type array with rule type String', (done: any) => {
+        expect(Comment.obj.downvotedBy.type[0]).to.have.property('type');
+        expect(Comment.obj.downvotedBy.type[0].type).to.equal(String);
         done();
     });
 });
